ce5ea2cc38e5b85232e2ca342ca4d1f2b5dfa918
Denis Migranov <d.migranov@g.nsu.ru>
2018-09-24 13:40:16 +0700

    c+++'ed it

--- CV_Lab1/Source.cpp
+++ CV_Lab1/Source.cpp
@@ -1,6 +1,8 @@
 #include <opencv2/opencv.hpp>
 
-void mouseCallback(int event, int x, int y, int flag, void * param)
+using namespace cv;
+
+void mouseCallback(int event, int x, int y, int flags, void * userdata)
 {
 	if (event == CV_EVENT_LBUTTONDOWN)
 		std::cout << x << " " << y << std::endl;
@@ -11,30 +13,16 @@ void mouseCallback(int event, int x, int y, int flag, void * param)
 
 int main(int argc, char **argv)
 {
-	IplImage* image = NULL;
-	IplImage* src = NULL;
-
-	/*cv::Mat img = cv::imread("image.jpg");
-	cv::imshow("img", img);
-	cv::waitKey(0);
-	return 0;*/
-
-	const char * filename = "image.jpg";
-	//char * dd = "fd";
-	src = cvLoadImage(filename, 1); //> 0 <=> RGB; ==0 <=> grayscale 
-	image = cvCloneImage(src);
-
-	cvNamedWindow("Lab1", CV_WINDOW_AUTOSIZE);
-	cvShowImage("Lab1", image);
-
-	cvSetMouseCallback("Lab1", mouseCallback);
 
 
+	Mat img = imread("image.jpg");
+	//imshow("img", img);
 
-	cvWaitKey(0);
-	cvReleaseImage(&image);
-	cvReleaseImage(&src);
-	cvDestroyWindow("Lab1");
+	setMouseCallback("Lab1", mouseCallback, NULL);
+
+	waitKey(0);
+
+
 
 	return 0;
 
