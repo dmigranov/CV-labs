25a18496ced225099cd1a7d486c8654e51149b9a
Denis Migranov <d.migranov@g.nsu.ru>
2018-09-30 13:33:52 +0700

    xyz and as as a result Lab are under question nonetheless, Lab is ready

--- CV_Lab1/Source.cpp
+++ CV_Lab1/Source.cpp
@@ -1,9 +1,26 @@
 #include <opencv2/opencv.hpp>
 
+#define Xn 95.04
+#define Yn 100.0
+#define Zn 108.88
+
 using namespace cv;
 
 Mat img;
 int hueSlider;
+
+
+double labfunction(double x)
+{
+	if (x > pow(6.0 / 29, 3))
+	{
+		return pow(x, 1.0 / 3);
+	}
+	else
+	{
+		return pow(29.0 / 6, 2) / 3 + 4.0 / 29;
+	}
+}
 
 void mouseCallback(int event, int x, int y, int flags, void * userdata)
 {
@@ -20,7 +37,7 @@ void mouseCallback(int event, int x, int y, int flags, void * userdata)
 		double mx = max(max(red, blue), green);
 		double mn = min(min(red, blue), green);
 
-		double value = mx / 256 * 100;
+		double value = mx / 255 * 100;
 		double saturation = mx != 0 ? (1 - mn / mx) * 100 : 0;
 		double hue;
 
@@ -37,8 +54,24 @@ void mouseCallback(int event, int x, int y, int flags, void * userdata)
 
 		std::cout << "HSV: " << hue << " " << saturation << " " << value << std::endl;
 		
+		//XYZ
+		Mat xyzmatrix = (Mat_<double>(3, 3) << 2.768892, 1.751748, 1.13016, 1.0, 4.5907, 0.0601, 0.0, 0.056508, 5.594292);
+		
+		Vec3d rgb(red, green, blue);
+		rgb /= 255; //rgb *= 100; //right??? //???????????
+		std::cout << xyzmatrix << std::endl;
+		Mat xyz = xyzmatrix * Mat(rgb);
+
+		//std::cout << xyz << std::endl;
+		//std::cout << xyz.at<double>(2, 0) << std::endl;
+		double x = xyz.at<double>(0, 0);
+		double y = xyz.at<double>(1, 0);
+		double z = xyz.at<double>(2, 0);
 		//LAB
-		//std::cout << "L*a*b*: " << L* << " " << a* << " " << b* << std::endl;
+		double L = 116 * labfunction(y / Yn) - 16; //rgb[1] = y
+		double a = 500 * (labfunction(x / Xn) - labfunction(y / Yn));
+		double b = 200 * (labfunction(y / Yn) - labfunction(z / Zn));
+		std::cout << "L*a*b*: " << L << " " << a << " " << b << std::endl;
 	}
 	
 }
@@ -84,8 +117,6 @@ int main(int argc, char **argv)
 
 
 	waitKey(0);
-
-
 
 	return 0;
 
