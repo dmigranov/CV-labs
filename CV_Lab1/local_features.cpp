ec01991fa4f1263895046545c08e6f2763f0dbcf
Денис  Мигранов <d.migranov@g.nsu.ru>
2018-11-24 18:40:40 +0700

    well, I guess that's it...

--- CV_Lab1/local_features.cpp
+++ CV_Lab1/local_features.cpp
@@ -173,70 +173,78 @@ Mat SIFT(Mat orig, double sigma)
 	//return gauss_DOG(orig, sigma, 5, 5);
 	for (int i = 0; i < 6; i++)
 	{
-		dogs[0][i] = gauss_DOG(copy, sigma, 13);
+		dogs[0][i] = gauss_DOG(copy, sigma, 5, i + 2);
+		dogs[1][i] = gauss_DOG(copy, sigma, 9, i + 2);
+		dogs[2][i] = gauss_DOG(copy, sigma, 13, i + 2);
 	}
-	for (int k = 1; k < 5; k++)
+	for (int s = 0; s < 3; s++)
 	{
-		for (int i = 0; i < orig.rows; i++)
-			for (int j = 0; j < orig.cols; j++)
-			{
-				uint moreCounter = 0;
-				uint lessCounter = 0;
-				uint desiredCount = 0;
-				for (int x = -1; x < 2; x++)
-					for (int y = -1; y < 2; y++)
-					{
-						if (i + x >= 0 && i + x < orig.rows && j + y >= 0 && j + y < orig.cols)
+		for (int k = 1; k < 5; k++)
+		{
+			for (int i = 0; i < orig.rows; i++)
+				for (int j = 0; j < orig.cols; j++)
+				{
+					uint moreCounter = 0;
+					uint lessCounter = 0;
+					uint desiredCount = 0;
+					for (int x = -1; x < 2; x++)
+						for (int y = -1; y < 2; y++)
 						{
-							//if (dogs[0][k - 1].at<Vec3b>(i, j) >= dogs[0][k].at<Vec3b>(i + x, j + y) && dogs[0][k].at<Vec3b>(i, j) >= dogs[0][k].at<Vec3b>(i + x, j + y) && dogs[0][k + 1].at<Vec3b>(i, j) >= dogs[0][k].at<Vec3b>(i + x, j + y))
-							if (more(dogs[0][k].at<double>(i, j), dogs[0][k - 1].at<double>(i + x, j + y)))
-								moreCounter++;
-							if((x != 0 || y != 0) && more(dogs[0][k].at<double>(i, j), dogs[0][k].at<double>(i + x, j + y)))
-								moreCounter++;
-							if (more(dogs[0][k].at<double>(i, j), dogs[0][k + 1].at<double>(i + x, j + y)))
-								moreCounter++;
+							if (i + x >= 0 && i + x < orig.rows && j + y >= 0 && j + y < orig.cols)
+							{
+								//if (dogs[0][k - 1].at<Vec3b>(i, j) >= dogs[0][k].at<Vec3b>(i + x, j + y) && dogs[0][k].at<Vec3b>(i, j) >= dogs[0][k].at<Vec3b>(i + x, j + y) && dogs[0][k + 1].at<Vec3b>(i, j) >= dogs[0][k].at<Vec3b>(i + x, j + y))
+								if (more(dogs[s][k].at<double>(i, j), dogs[s][k - 1].at<double>(i + x, j + y)))
+									moreCounter++;
+								if ((x != 0 || y != 0) && more(dogs[s][k].at<double>(i, j), dogs[s][k].at<double>(i + x, j + y)))
+									moreCounter++;
+								if (more(dogs[s][k].at<double>(i, j), dogs[s][k + 1].at<double>(i + x, j + y)))
+									moreCounter++;
 
-							if (less(dogs[0][k].at<double>(i, j), dogs[0][k - 1].at<double>(i + x, j + y)))
-								lessCounter++;
-							if ((x != 0 || y != 0 ) && less(dogs[0][k].at<double>(i, j), dogs[0][k].at<double>(i + x, j + y)))
-								lessCounter++;
-							if (less(dogs[0][k].at<double>(i, j), dogs[0][k + 1].at<double>(i + x, j + y)))
-								lessCounter++;
-							
+								if (less(dogs[s][k].at<double>(i, j), dogs[s][k - 1].at<double>(i + x, j + y)))
+									lessCounter++;
+								if ((x != 0 || y != 0) && less(dogs[s][k].at<double>(i, j), dogs[s][k].at<double>(i + x, j + y)))
+									lessCounter++;
+								if (less(dogs[s][k].at<double>(i, j), dogs[s][k + 1].at<double>(i + x, j + y)))
+									lessCounter++;
+
+							}
 						}
-					}
-				//if(moreCounter >= 25)
-					//std::cout << moreCounter << std::endl;
-				if (moreCounter == 26 || lessCounter == 26) //лок макс
-					ret.at<Vec3b>(i, j) = 0;
-					
-			}
+					/*if(moreCounter >= 25)
+						std::cout << moreCounter << std::endl;*/
+					if (moreCounter >= 26 || lessCounter >= 26) //лок макс
+						ret.at<Vec3b>(i, j) = 0;
+
+				}
+		}
 	}
 	return ret;
 }
 
 
 //filter_size = 2n+1!
-Mat gauss_DOG(Mat &original, double sigma, int filterSize)
+Mat gauss_DOG(Mat &original, double sigma, int filterSize, int k)
 {
 	Mat orig;
 	original.convertTo(orig, CV_64F);
 	Mat newimg(orig.rows, orig.cols, orig.type());
 	newimg = 0;
 	Mat gauss(filterSize, filterSize, CV_64FC1);
-	double div = 0;
+	Mat kgauss(filterSize, filterSize, CV_64FC1);
+	double div = 0, kdiv = 0;
 
 	for (int x = -filterSize / 2; x < filterSize / 2 + 1; x++)
 		for (int y = -filterSize / 2; y < filterSize / 2 + 1; y++)
 		{
 			//k раз применить фильтр с сигма эквивалентно ли одному k*сигма?
 			gauss.at<double>(x + filterSize / 2, y + filterSize / 2) = exp(-(x * x + y * y) / (2.0 * sigma * sigma)) / (2.0 * M_PI * sigma * sigma);
-			//kgauss.at<double>(x + filterSize / 2, y + filterSize / 2) = exp(-(x * x + y * y) / (2.0 * k * k *sigma * sigma)) / (2.0 * M_PI * k * k * sigma * sigma);
+			kgauss.at<double>(x + filterSize / 2, y + filterSize / 2) = exp(-(x * x + y * y) / (2.0 * k * k *sigma * sigma)) / (2.0 * M_PI * k * k * sigma * sigma);
 			div += gauss.at<double>(x + filterSize / 2, y + filterSize / 2);
-			//kdiv += kgauss.at<double>(x + filterSize / 2, y + filterSize / 2);
+			kdiv += kgauss.at<double>(x + filterSize / 2, y + filterSize / 2);
 		}
 	gauss /= div;
-	//kgauss /= kdiv;
+	kgauss /= kdiv;
+
+	gauss = kgauss - gauss; //
 	//Dog(x, y, sigma) = (G(k*sigma) - G(sigma)) * I
 
 	for (int i = 0; i < orig.rows; i++)
@@ -255,12 +263,14 @@ Mat gauss_DOG(Mat &original, double sigma, int filterSize)
 				}
 		}
 
-	Mat ret = newimg - orig;
+	/*Mat ret = newimg - orig;
 	newimg.convertTo(newimg, original.type());
 	ret.convertTo(ret, original.type());
 	original = newimg;
 
-	return ret;
+	return ret;*/
+	newimg.convertTo(newimg, original.type());
+	return newimg;
 }
 
 bool more(double v1, double v2)
